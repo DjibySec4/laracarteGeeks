6947cfff0af3de58c784fb1e989d74bc805e6670
Ally <alysck@gmail.com>
2019-11-11 18:41:23 +0000

    Configure app name

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="text-center">
-   <p> &copy; {{ date('Y')}} - Laracarte by <a href="https://twiter.com/djibysec">@DjibySeck</a> - Laramap clone app. </p>
+   <p> &copy; {{ date('Y')}} - {{ config('app.name') }} by <a href="https://twiter.com/djibysec">@DjibySeck</a> - Laramap clone app. </p>
    <p> <b> <strong> This app has been build for learning purposes. </strong> </b></p>
 </footer>
